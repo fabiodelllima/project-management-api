748b4554f446c098d53c9fe4b2a7d2b99caaeefb
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-04 20:43:31 -0300

    chore: Indentação e detalhes no código redefinidos

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application } from "express";
-import "dotenv/config";
+import express, { Application } from 'express';
+import 'dotenv/config';
 
 const app: Application = express();
 
