0f4a7b4ae05f257424255f611874da63c8d09e4a
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-15 05:38:38 -0300

    feat: Rotas de Developers

--- src/routes/developers.route.ts
+++ src/routes/developers.route.ts
@@ -1,6 +1,47 @@
 import { Router } from 'express';
-import { createDeveloperService } from '../services/developers.service';
+import { verifyEmail } from '../middlewares/verifyEmail.middleware';
+import { verifyDeveloperId } from './../middlewares/verifyDeveloperId.middleware';
+import { createDeveloperInfosController } from '../controllers/developerInfos.controller';
+import {
+  createDeveloperController,
+  deleteDeveloperController,
+  readDeveloperByIdController,
+  updateDeveloperController,
+} from '../controllers/developers.controller';
+import { verifyOS } from '../middlewares/verifyOS.middleware';
+import { verifyDeveloperInfo } from '../middlewares/verifyDeveloperInfo.middleware';
 
 export const developersRoutes: Router = Router();
 
-developersRoutes.post('/', createDeveloperService);
+developersRoutes.post(
+  '/',
+  verifyEmail,
+  createDeveloperController
+);
+
+developersRoutes.get(
+  '/:id',
+  verifyDeveloperId,
+  readDeveloperByIdController
+);
+
+developersRoutes.patch(
+  '/:id',
+  verifyEmail,
+  verifyDeveloperId,
+  updateDeveloperController
+);
+
+developersRoutes.delete(
+  '/:id',
+  verifyDeveloperId,
+  deleteDeveloperController
+);
+
+developersRoutes.post(
+  '/:id/infos',
+  verifyDeveloperInfo,
+  verifyDeveloperId,
+  verifyOS,
+  createDeveloperInfosController
+);
